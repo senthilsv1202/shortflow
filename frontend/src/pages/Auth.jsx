ce75aeee8a26cbe055267c21780026aea430737c
senthilsv1202 <contactss1202@gmail.com>
2026-06-11 14:59:59 -0400

    Update Auth.jsx

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -170,4 +170,8 @@ export default function Auth() {
       </div>
     </div>
   )
+
+
+  console.log('URL:', import.meta.env.VITE_SUPABASE_URL)
+ console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY)
 }
